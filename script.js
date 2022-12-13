c00353c11a713ef7a265e5a9036fe343be6c9bcb
Dhananjay Chourasiya <57860395+dhananjaychourasiya@users.noreply.github.com>
2022-12-14 01:21:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButtons = document.querySelector('[data-equals]')
+const deleteButtons = document.querySelector('[data-delete]')
+const allClearButtons = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
